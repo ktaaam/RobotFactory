3e7864dcf4a23f31ad4ce259367cf736a6ab0e90
ktaaam <kevint4m@gmail.com>
2017-02-12 14:06:43 -0800

    Added dash board to home page, changed css to nav, changed html in about page

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -1,10 +1,8 @@
-<?php
-/*
- * Menu navbar, just an unordered list
- */
-?>
-<ul class="nav">
-    {menudata}
-    <li><a href="{link}">{page}</a></li>
-    {/menudata}
-</ul>
+<ul class="nav nav-pills" >
+	<li> <a href="index.html" class="logo"><b>Dankpapaya</b></a></li>
+
+      {menudata}
+ 	 <li ><a href="{link}">{page}</a></li>
+
+      {/menudata}
+  </ul>
