2871a56a54249bb13cbc122886965e222a12173a
ktaaam <kevint4m@gmail.com>
2017-02-12 13:11:58 -0800

    Synch with team repo
    Signed-off-by: ktaaam <kevint4m@gmail.com>

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -3,8 +3,9 @@
  * Menu navbar, just an unordered list
  */
 ?>
-<ul class="nav">
+<ul class="nav nav-pills" >
+	<li> <a href="index.html" class="logo"><b>Dankpapaya</b></a></li>
     {menudata}
-    <li><a href="{link}">{page}</a></li>
+    <li ><a href="{link}">{page}</a></li>
     {/menudata}
 </ul>
